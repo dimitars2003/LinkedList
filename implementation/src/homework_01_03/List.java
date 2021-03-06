d390ffa9bc3d3366f1329577dfeb041a5352a30d
Admin <Admin@DESKTOP-RSJJARR>
2021-03-06 18:59:28 +0200

    node made and start of linked list implementation

---
+++ implementation/src/homework_01_03/List.java
@@ -0,0 +1,12 @@
+package homework_01_03;
+
+public interface List <T> {
+	boolean isEmpty();
+	boolean contains(T item);
+	void add(T item);
+	void insertAt(T item, int index);
+	void removeFrom(int index);
+	void remove(T item);
+	T get(int index);
+	void clear();
+}
